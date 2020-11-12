22c422ff487364003af8357e858bd2a1a2c02c72
Frank Buono <fbuonojr@fordham.edu>
2020-11-12 10:37:10 -0500

    finished buildQueryURL function

--- script.js
+++ script.js
@@ -2,27 +2,26 @@
  * this function takes info from the form and makes the query url
  * @returns {string}
  */
-function buildQueryURl() {
+function buildQueryURL() {
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
-    var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M"};
+    var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
 
     queryParams.q = $("#search-term").val().trim();
 
     var startYear = $("#start-year").val().trim();
 
-    if(parseInt(startYear)) {
+    if (parseInt(startYear)) {
         queryParams.begin_date = startYear + "0101";
     }
 
     var endYear = $("#end-year").val().trim();
 
-    if(parseInt(endYear)){
+    if (parseInt(endYear)) {
         queryParams.end_date = endYear + "0101";
     }
 
     console.log("---------------\nURL: " + queryURL + "\n---------------");
     console.log(queryURL + $.param(queryParams));
-  return queryURL + $.param(queryParams);
-
+    return queryURL + $.param(queryParams);
 }
