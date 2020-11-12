24580c282f68545b55b3874865a98b2da75097f2
Frank Buono <fbuonojr@fordham.edu>
2020-11-11 21:21:32 -0500

    started query function

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ * this function takes info from the form and makes the query url
+ * @returns {string}
+ */
+function buildQueryURl() {
+    var qyeryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+}
