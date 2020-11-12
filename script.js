77c56ca361235ed83071c6be0a46bd86954a98d8
Frank Buono <fbuonojr@fordham.edu>
2020-11-12 10:08:28 -0500

    setting up parameters for queryURL

--- script.js
+++ script.js
@@ -3,5 +3,21 @@
  * @returns {string}
  */
 function buildQueryURl() {
-    var qyeryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+    var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M"};
+
+    queryParams.q = $("#search-term").val().trim();
+
+    var startYear = $("#start-year").val().trim();
+
+    if(parseInt(startYear)) {
+        queryParams.begin_date = startYear + "0101";
+    }
+
+    var endYear = $("#end-year").val().trim();
+
+    if(parseInt(endYear)){
+        queryParams.end_date = endYear + "0101";
+    }
 }
